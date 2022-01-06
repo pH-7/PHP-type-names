3bd89b38cf3e0e423aaff9ecdbc71ed386e451c4
Pierre-Henry Soria <hi@ph7.me>
2022-01-06 20:34:56 +1030

    Creating datatype package :package:

---
+++ src/Types.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author      Pierre-Henry Soria <hi@ph7.me>
+ * @copyright   (c) 2022, Pierre-Henry Soria
+ * @license     MIT License; <https://opensource.org/licenses/MIT>
+ */
+
+namespace Data;
+
+final class Types
+{
+    public const BOOLEAN = 'boolean';
+    public const INTEGER = 'int';
+    public const FLOAT = 'float';
+    public const STRING = 'string';
+    public const DATE = 'date';
+}
